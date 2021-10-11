970d4d56d92df8643294678dbd912c2d9f31f692
Ahmet OZKESEK <aozkesek@gmail.com>
2021-10-11 21:32:02 +0100

    fix stockapp image

--- pod/stocks-create.sh
+++ pod/stocks-create.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-POD="-n stocks -p 9080:9080 -p 6379:6379"
+POD="-n stocks -p 6380:6380"
 # containers in the pod
 RDS="--pod stocks --name redissrv -v stock_data:/data "
-APP="--pod stocks --name stockapp -v stock_apps:/var/www/stocks --expose 9080 --env APP_OPTS=\"-Dserver.port=9080\""
+APP="--pod stocks --name stockapp -v stock_apps:/var/www/stocks --expose 6380 \
+	--env APP_OPTS=\"-Dserver.port=6380\""
 
 # app-server start stocks app
 CMD="/var/www/stocks/app/bin/app "
